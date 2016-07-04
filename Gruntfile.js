bc51aa0fcd161e3c614ba2ec91d006877a85d5d1
shu mai <ansiboy@163.com>
2016-07-04 17:36:32 +0800

    实现首页

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+var dest_root = 'www';
+var src_root = 'src';
+var ts_options = {
+    module: 'amd',
+    target: 'es5',
+    removeComments: true,
+    references: [
+        src_root + "/js/typings/*.d.ts"
+    ],
+    sourceMap: false,
+};
+module.exports = function(grunt) {
+    grunt.initConfig({
+        ts: {
+            app: {
+                src: [src_root + '/**/*.ts'],
+                dest: dest_root,
+                options: ts_options
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    { expand: true, cwd: src_root, src: ['**/*.html'], dest: dest_root },
+                    { expand: true, cwd: src_root, src: ['js/**/*.js'], dest: dest_root },
+                    { expand: true, cwd: src_root, src: ['css/**/*.css'], dest: dest_root },
+                    { expand: true, cwd: src_root, src: ['font/**/*.*'], dest: dest_root }
+                ]
+            }
+        },
+        less: {
+            app: {
+                files: [{
+                    expand: true,
+                    cwd: src_root + '/css/app',
+                    src: ['**/*.less'],
+                    dest: dest_root + '/css/app',
+                    ext: '.css'
+                }]
+            },
+            // bootstrap: {
+            //     files: [{
+            //         src: [src_root + '/css/bootstrap-3.3.5/bootstrap.less'],
+            //         dest: dest_root + '/css/bootstrap.css'
+            //     }]
+            // },
+            // chitu: {
+            //     files: [{
+            //         src: [src_root + '/css/chitu.less'],
+            //         dest: dest_root + '/css/chitu.css'
+            //     }]
+            // }
+        }
+    });
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('default', ['ts', 'copy', 'less']);
+}
---
+++ src/application.ts
@@ -0,0 +1,4 @@
+import chitu = require('chitu');
+let app = new chitu.Application();
+app.run();
+export = app;

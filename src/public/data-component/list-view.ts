30b8aa61053e666cc9967268dee40cf89ad8f587
Shu Mai <ansiboy@163.com>
2019-07-12 08:29:16 +0800

    清理注释代码

--- src/public/data-component/list-view.ts
+++ src/public/data-component/list-view.ts
@@ -5,18 +5,6 @@ import { GridView } from "maishu-wuzhui"
 import { loadControlModule } from "./common";
 import { Resource } from "entities";
 import { PageView, PageViewArguments } from "./page-view";
-// import { PermissionService } from "assert/services/index";
-
-// type ListViewArguments<T> = {
-//     element: HTMLElement,
-//     dataSource: DataSource<T>,
-//     columns: DataControlField<T>[],
-//     menuItems: MenuItem[],
-//     resourceId: string,
-//     pageSize?: number,
-//     transform?: (items: T[]) => T[],
-//     context?: object,
-// }
 
 type ListViewArguments<T> = {
     pageSize?: number,
@@ -31,39 +19,15 @@ export class ListView<T> extends PageView {
     constructor(args: ListViewArguments<T>) {
         super(args);
         let resource = args.menuItems.filter(o => o.id == args.resourceId)[0];
-        // console.assert(resource != null);
-
-        // this.initElement(args, resource);
 
         let parentDeep = this.parentDeep(resource);
         if (parentDeep > 1) {
             this.showBackButton();
         }
 
-        // let navBar = args.element.querySelector("ul");
-        // this.loadTopControls(args).then(controls => {
-        //     controls.reverse();
-        //     controls.map(ctrl => {
-        //         let li = document.createElement("li");
-        //         li.className = "pull-right";
-        //         li.appendChild(ctrl);
-        //         return li;
-
-        //     }).forEach(li => {
-        //         navBar.appendChild(li);
-        //     })
-
-        // })
-
-
-        // let tableElement = this.createGridView(args);
-        // args.element.appendChild(tableElement);
-
     }
 
     protected render(element) {
-        // let listView = this as any as ListView<T>;
-        // listView.createGridView(args)
         this.createGridView(element, this.args as ListViewArguments<T>)
     }
 
@@ -81,25 +45,6 @@ export class ListView<T> extends PageView {
 
         return deep;
     }
-
-    // private initElement(args: ListViewArguments<T>, menuItem: MenuItem) {
-    //     let { element } = args;
-    //     element.innerHTML = `
-    //     <div class="tabbable">
-    //         <ul class="nav nav-tabs" style="min-height:34">
-    //             <li className="pull-left">
-    //                 <div style="font-weight: bold; font-size: 16px">${menuItem.name}</div>
-    //             </li>
-    //             <li class="pull-right">
-    //                 <button class="btn btn-primary pull-right" style="display:none;">
-    //                     <i class="icon-reply"></i>
-    //                     <span>返回</span>
-    //                 </button>
-    //             </li>
-    //         </ul>
-    //     </div>
-    //     `
-    // }
 
     private createGridView<T>(container: HTMLElement, args: ListViewArguments<T>): GridView<T> {
         let tableElement = document.createElement("table");
@@ -161,25 +106,8 @@ export class ListView<T> extends PageView {
             container.appendChild(tableElement);
         }
 
-
-
         return _gridView;
     }
 
-
-    // private async loadTopControls(args: ListViewArguments<T>): Promise<HTMLElement[]> {
-    //     let resource_id = args.resourceId;
-    //     if (!resource_id) return null
-
-    //     let resources = args.menuItems;
-    //     let menuItem = resources.filter(o => o.id == resource_id)[0];
-    //     console.assert(menuItem != null)
-    //     let menuItemChildren = resources.filter(o => o.parent_id == menuItem.id);
-    //     let controlResources = menuItemChildren.filter(o => o.data != null && o.data.position == "top");
-    //     let controlFuns = await Promise.all(controlResources.map(o => loadControlModule(o.page_path)));
-
-    //     let controls = controlFuns.map((func, i) => func({ resource: controlResources[i], dataItem: {}, context: args.context }));
-    //     return controls;
-    // }
 }
 

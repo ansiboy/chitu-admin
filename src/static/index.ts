7212c4cab471e1e93adfbe100e90788f45e80d48
Shu Mai <ansiboy@163.com>
2019-09-23 17:07:19 +0800

    update

--- src/static/index.ts
+++ src/static/index.ts
@@ -1,5 +1,4 @@
 export { InitArguments } from "./assert/startup";
 import errorHandle1 from "./error-handle";
 export let errorHandle = errorHandle1;
-export { DataListPage } from "./assert/components/data-list-page";
 export { WebSiteConfig } from "./assert/config";
